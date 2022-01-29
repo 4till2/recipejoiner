120fe226635464ea28b1a7778ac2919dbc4f8ef9
Yosef Serkez <yosefserkez@gmail.com>
2022-01-29 16:19:53 -0500

    styles: recipes and cookbooks feed and show styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :turbo_frame_request_variant
+  # before_action :turbo_frame_request_variant
 
   def user_not_authorized
     flash[:alert] = 'You are not authorized to perform this action.'
@@ -11,10 +11,10 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def turbo_frame_request_variant
-    request.variant = :turbo_frame if turbo_frame_request?
-    # request.format = :turbo_frame if turbo_frame_request?
-  end
+  # def turbo_frame_request_variant
+  #   request.variant = :turbo_frame if turbo_frame_request?
+  #   # request.format = :turbo_frame if turbo_frame_request?
+  # end
 
   protected
 
