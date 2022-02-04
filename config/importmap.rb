72097891ba259354b345a72468705cdd39641804
Yosef Serkez <yosefserkez@gmail.com>
2022-02-04 17:14:38 -0500

    fix: add utils to import map

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,4 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin 'app/javascript/utils', to: 'utils.js', preload: true
+pin 'utils', preload: true
